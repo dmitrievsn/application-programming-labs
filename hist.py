076ebbf449d96f55c35a795d9acf90596db5361b
dmitrievsn <163391506+dmitrievsn@users.noreply.github.com>
2024-10-19 14:42:44 +0400

    Update hist.py

--- hist.py
+++ hist.py
@@ -4,29 +4,33 @@ import cv2
 from numpy import ndarray
 
 
-def histogram_creation(img: ndarray)-> ndarray:
+def histogram_creation(img: ndarray)-> tuple:
     """
     Функция создает гистограмму цветного изображения
     :param img:массив из пикселей(цветное изображение в формате rgb)
-    :return:гистограмма по всем каналам,содержащая количество пикселей для каждого из 256 уровней яркости
+    :return:гистограмма для каждого канала,содержащая количество пикселей для каждого из 256 уровней яркости
     """
     hist_b = cv2.calcHist([img], [0], None, [256], [0, 256])
     hist_g = cv2.calcHist([img], [1], None, [256], [0, 256])
     hist_r = cv2.calcHist([img], [2], None, [256], [0, 256])
-    hist=hist_r+hist_g+hist_b
-    return hist
+    return hist_r,hist_g,hist_b
 
 
-def histogram_drawing(histogram: ndarray) -> None:
+def histogram_drawing(hist_r: ndarray, hist_g: ndarray, hist_b: ndarray) -> None:
     """
     Функция рисует гистограмму на основе переданного массива
-    :param histogram:массив с количеством пикселей для каждого уровня яркости(0-255)
+    :param hist_r: данные для гистограммы красного канала
+    :param hist_g: данные для гистограммы зеленого канала
+    :param hist_b: данные для гистограммы синего канала
     :return: None
     """
     plt.figure(figsize=(10, 5))
-    plt.plot(histogram)
+    plt.plot(hist_r, color='red', label='Красный канал')
+    plt.plot(hist_g, color='green', label='Зеленый канал')
+    plt.plot(hist_b, color='blue', label='Синий канал')
     plt.xlim([0, 256])
     plt.title('Гистограмма изображения')
     plt.xlabel('Интенсивность пикселей')
     plt.ylabel('Количество пикселей')
+    plt.legend()
     plt.show()
