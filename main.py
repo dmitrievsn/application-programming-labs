2e5debc7573f3212510f575af52fbf818081db81
dmitrievsn <163391506+dmitrievsn@users.noreply.github.com>
2024-10-19 14:37:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def main():
     path_im=value_input().path_image
     path_new_im=value_input().path_new_image
     try:
+        if not os.path.exists(path_im):
+            raise FileNotFoundError(f"Файл не найден: {path_im}")
         img = cv2.imread(path_im)
+        if img is None:
+            raise ValueError("Не удалось загрузить изображение. Проверьте путь.")
         dimensioning(path_im)
-        histogram_drawing(histogram_creation(img))
+        hist_r,hist_g,hist_b=histogram_creation(img)
+        histogram_drawing(hist_r,hist_g,hist_b)
         new_im=grayscale_image(img)
         save_grayscale_image(new_im, path_new_im)
         image_output(img,new_im)
